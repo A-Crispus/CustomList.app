8b05a4aabe2427cbf747b4643f1de618dd67912b
augustrayziss <augustrayziss@gmail.com>
2024-05-22 22:17:23 +0300

    solved problem

--- app/src/main/java/com/example/customlistview/Model.kt
+++ app/src/main/java/com/example/customlistview/Model.kt
@@ -1,6 +1,3 @@
 package com.example.customlistview
 
-import android.media.Image
-
-class Model(val tittle:String,val description:String,val image:Int) {
-}
+class Model(val tittle:String,val description:String,val image:Int)
